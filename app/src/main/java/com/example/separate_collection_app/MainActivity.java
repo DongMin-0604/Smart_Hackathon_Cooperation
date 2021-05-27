ad5709ef4d6e3ecd694032bf31956c2450c42a48
DongMin-0604 <dongmin3147@gmail.com>
2021-05-27 11:10:15 +0900

    중간 점검용 커밋 시작화면에서 홈화면 연결 홈화면에 하단 네비게이션 바 구현

--- app/src/main/java/com/example/separate_collection_app/MainActivity.java
+++ app/src/main/java/com/example/separate_collection_app/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.separate_collection_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new Search()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_fragment1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Search()).commit();
+                        break;
+                    case R.id.item_fragment2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new Home()).commit();
+                        break;
+                    case R.id.item_fragment3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyInfo()).commit();
+                        break;
+                    }
+                return true;
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/separate_collection_app/StartActivity.java
+++ app/src/main/java/com/example/separate_collection_app/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.separate_collection_app;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,14 @@ public class StartActivity extends AppCompatActivity {
         start_title =(TextView) findViewById(R.id.Start_Title);
         start_title.setPaintFlags(start_title.getPaintFlags()| Paint.FAKE_BOLD_TEXT_FLAG);
 
+        Button TestBT = (Button)findViewById(R.id.go_main_bt);
+        TestBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+            }
+        });
 
     }
 }
