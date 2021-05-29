813187a35a657f63e369e87b1a741f0cfb26f478
DongMin-0604 <dongmin3147@gmail.com>
2021-05-29 15:49:53 +0900

    홈화면 날짜 표시 기능 추가

--- app/src/main/java/com/example/separate_collection_app/Home.java
+++ app/src/main/java/com/example/separate_collection_app/Home.java
@@ -1,20 +1,49 @@
 package com.example.separate_collection_app;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Home extends Fragment {
 
+    TextView TV_Date;
+    long Time;
+    Date mDate;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+
+        TV_Date = (TextView)v.findViewById(R.id.Data_Display);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        TV_Date.setText("오늘은 "+getTime()+" 입니다.");
+
+        return v;
     }
+
+    private String getTime(){
+        Time = System.currentTimeMillis();
+        mDate = new Date(Time);
+        return mFormat.format(mDate);
+    }
+
+
+
+
 }
