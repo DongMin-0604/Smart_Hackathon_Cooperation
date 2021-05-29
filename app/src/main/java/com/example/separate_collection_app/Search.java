db99684c009d99b4284a09429d8cacd9d377912b
minju0213 <mjni1214@naver.com>
2021-05-29 17:50:48 +0900

    검색바 추가 , 버튼 추가

--- app/src/main/java/com/example/separate_collection_app/Search.java
+++ app/src/main/java/com/example/separate_collection_app/Search.java
@@ -2,9 +2,12 @@ package com.example.separate_collection_app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -17,4 +20,6 @@ public class Search extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+
 }
